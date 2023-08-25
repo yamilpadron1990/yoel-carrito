e209a3bff3a41ba285a3b5f2d8ffefd495c9900e
yoelhcamejo <yoelhcamejo@gmail.com>
2023-08-25 21:39:42 +0200

    Inicio del proyecto con Maven

---
+++ tienda-online-web/src/main/java/com/yoelhcamejo/tiendaonline/tiendaonlineweb/app/TiendaOnlineWebApplication.java
@@ -0,0 +1,13 @@
+package com.yoelhcamejo.tiendaonline.tiendaonlineweb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaOnlineWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaOnlineWebApplication.class, args);
+	}
+
+}
---
+++ tienda-online-web/src/test/java/com/yoelhcamejo/tiendaonline/tiendaonlineweb/app/TiendaOnlineWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoelhcamejo.tiendaonline.tiendaonlineweb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaOnlineWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
